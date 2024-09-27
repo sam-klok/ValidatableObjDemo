a85ef3f9307850c26a76acdfccbad8677cba6914
sam-klok <sklok11@gmail.com>
2024-09-26 19:56:50 -0700

    Add project files.

---
+++ ValidatableObjDemo/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatableObjDemo
+{
+    public enum Category
+    {
+        Best = 1,
+        Ok = 2,
+        Bad = 3,
+    }
+}
---
+++ ValidatableObjDemo/Settings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatableObjDemo
+{
+    public class Settings: IValidatableObject
+    {
+        [Required]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Category Category { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!Enum.IsDefined(typeof(Category), Category))
+            {
+                yield return new ValidationResult(
+                    "Invalid Category", new[] { nameof(Category) });
+            }
+        }
+    }
+}
